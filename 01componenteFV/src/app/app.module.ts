fbf9af3c0a399b487816d1e507dacaa721c71f9b
Fermin Vazquez <fv.shutter@gmail.com>
2022-11-24 15:44:40 -0300

    Primer entrega

--- 01componenteFV/src/app/app.module.ts
+++ 01componenteFV/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContentCtnComponent } from './components/content-ctn/content-ctn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidebarComponent,
+    ContentCtnComponent
   ],
   imports: [
     BrowserModule,
---
+++ 01componenteFV/src/app/components/content-ctn/content-ctn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-ctn',
+  templateUrl: './content-ctn.component.html',
+  styleUrls: ['./content-ctn.component.css']
+})
+export class ContentCtnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
